bc2bd5a94ccfbc22e03463f75924ad535a3359df
Mayar-Abdulkareem <mayarimad70@gmail.com>
2023-11-05 21:44:48 +0200

    coordinator added

---
+++ GP_iOS/Coordinators/MainCoordinator.swift
@@ -0,0 +1,34 @@
+//
+//  MainCoordinator.swift
+//  GP_iOS
+//
+//  Created by Mayar Abdulkareem - FTS on 05/11/2023.
+//
+
+import UIKit
+
+/// The initial coordinator that decide which ViewController will will be shown first
+class MainCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinators = [Coordinator]()
+    
+    /// - Parameter navigationController: Used for managing the hierarchy
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.navigationController.isNavigationBarHidden = true
+    }
+    
+    /// which screen will be shown first
+    func start() {
+        showLoginFlow()
+    }
+    
+    ///  Start ``LoginCordinator`` to present the Login page
+    private func showLoginFlow() {
+        let coordinator = LoginCoordinator(navigationController: navigationController)
+        coordinator.parentCoordinator = self
+        self.childCoordinators.append(coordinator)
+        coordinator.start()
+    }
+}
---
+++ GP_iOS/Screens/Login/Controllers/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  GP_iOS
+//
+//  Created by FTS on 05/11/2023.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    weak var coordinator: LoginCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
